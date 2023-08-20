34ed70b1f116fa65925534b02e15a4bf794d5041
aashut0xh <aashut0xh@gmail.com>
2023-08-20 20:36:31 +0530

    first commit

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a number :");
+        double r;
+        r=sc.nextDouble();
+        double pi=3.14;
+        double area= r*pi*pi;
+        System.out.println("AREA IS GIVEN "+area);
+
+    }
+
+
+}
+
