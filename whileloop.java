bb68c6feb3376309e17927ca3d1a091a34f8b96d
aashut0xh <aashut0xh@gmail.com>
2023-08-24 19:37:43 +0530

    update

--- whileloop.java
+++ whileloop.java
@@ -1,5 +1,9 @@
 public class whileloop {
     public static void main(String[] args) {
-        
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;//i=i+1;
+        }
     }
 }
