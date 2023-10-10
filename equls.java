ec0dd098500488b10ac28c7be45d72c4d8e292bc
aashut0xh <aashut0xh@gmail.com>
2023-10-10 09:45:21 +0530

    update

---
+++ equls.java
@@ -0,0 +1,7 @@
+public class equls {
+    public static void main(String[] args) {
+        String a="HELLO BABY";
+        String b="HELLO BABY";
+        System.out.println(a.equals(b));
+    }
+}
